afea77268678453b7d1b03894295fe8a8ea6a6e6
Jensen <jensenmourat@gmail.com>
2021-04-17 13:54:27 +0200

    1.0.5

--- src/helper/hashMap.ts
+++ src/helper/hashMap.ts
@@ -1,50 +1,50 @@
-import {mergeMap} from './mergeMap';
+import { mergeMap } from './mergeMap';
 
 interface IHashMap<T, U> {
-    set(key: T, val: U): IHashMap<T, U>;
+  set(key: T, val: U): IHashMap<T, U>;
 
-    get(key: T): U | undefined;
+  get(key: T): U | undefined;
 
-    internalMap(): Map<string, U>;
+  internalMap(): Map<string, U>;
 
-    concat(...maps: HashMap<T, U>[]): HashMap<T, U>;
+  concat(...maps: HashMap<T, U>[]): HashMap<T, U>;
 
-    forEach(fn: (key: T, value: U) => void): void;
+  forEach(fn: (key: T, value: U) => void): void;
 }
 
 // A map which allows object keys
 export class HashMap<T, U> implements IHashMap<T, U> {
-    private map = new Map<string, U>();
+  private map = new Map<string, U>();
 
-    constructor() {
-        return this;
-    }
+  constructor() {
+    return this;
+  }
 
-    set(key: T, val: U): this {
-        const _key = JSON.stringify(key);
-        this.map.set(_key, val);
-        return this;
-    }
+  set(key: T, val: U): this {
+    const _key = JSON.stringify(key);
+    this.map.set(_key, val);
+    return this;
+  }
 
-    get(key: T): U | undefined {
-        const _key = JSON.stringify(key);
-        return this.map.get(_key);
-    }
+  get(key: T): U | undefined {
+    const _key = JSON.stringify(key);
+    return this.map.get(_key);
+  }
 
-    internalMap(): Map<string, U> {
-        return this.map;
-    }
+  internalMap(): Map<string, U> {
+    return this.map;
+  }
 
-    concat(...maps: HashMap<T, U>[]) {
-        const mapArr = maps.map((m) => m.internalMap());
-        this.map = mergeMap(...mapArr, this.map);
-        return this;
-    }
+  concat(...maps: HashMap<T, U>[]) {
+    const mapArr = maps.map((m) => m.internalMap());
+    this.map = mergeMap(...mapArr, this.map);
+    return this;
+  }
 
-    forEach(fn: (key: T, value: U) => void) {
-        this.map.forEach((v, k) => {
-            const key = JSON.parse(k) as T;
-            fn(key, v);
-        });
-    }
+  forEach(fn: (key: T, value: U) => void) {
+    this.map.forEach((v, k) => {
+      const key = JSON.parse(k) as T;
+      fn(key, v);
+    });
+  }
 }
