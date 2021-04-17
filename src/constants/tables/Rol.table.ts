2c862e1834018395a57d7172f8adf12511a95d7c
Jensen <jensenmourat@gmail.com>
2021-04-17 14:00:17 +0200

    tslint fix

--- src/constants/tables/Rol.table.ts
+++ src/constants/tables/Rol.table.ts
@@ -2,7 +2,7 @@ import { HashMap } from '../../helper/hashMap';
 import { OpCode, Operation } from './index';
 
 export const ROL_TABLE = new HashMap<OpCode, Operation>()
-  .set({ opcode: 'D0', modRm: '0' }, { operation: 'rol', op1: 'm8', bconstant: '1' })
+  .set({ opcode: 'D0', modRm: '0' }, { operation: 'rol', op1: 'm8', constant: '1' })
   .set({ opcode: 'D2', modRm: '0' }, { operation: 'rol', op1: 'm8', op2: 'cl', isRegisterIncluded: true })
   .set({ opcode: 'C0', modRm: '0' }, { operation: 'rol', op1: 'm8', op2: 'imm8' })
   .set({ opcode: 'D1', modRm: '0' }, { operation: 'rol', op1: 'm32', constant: '1', has16Bit: true })
